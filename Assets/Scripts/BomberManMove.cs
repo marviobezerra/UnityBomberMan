7168dd9519af6537fb727a8aecd6a304cd020a8f
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2018-02-11 06:55:22 -0500

    Creating world

--- Assets/Scripts/BomberManMove.cs
+++ Assets/Scripts/BomberManMove.cs
@@ -4,43 +4,53 @@ using UnityEngine;
 
 public class BomberManMove : MonoBehaviour
 {
-
+    public float moveSpeed = 5;
     private Animator animator;
+    private Rigidbody2D body;
     const string Position = "Position";
     const string Walk = "Walk";
 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (Input.GetKey("up"))
         {
+            body.velocity = new Vector2(0, moveSpeed);
+
             animator.SetBool(Walk, true);
             animator.SetInteger(Position, 0);
         }
         else if (Input.GetKey("right"))
         {
+            body.velocity = new Vector2(moveSpeed, 0);
+
             animator.SetBool(Walk, true);
             animator.SetInteger(Position, 1);
 
         }
         else if (Input.GetKey("down"))
         {
+            body.velocity = new Vector2(0, -moveSpeed);
+
             animator.SetBool(Walk, true);
             animator.SetInteger(Position, 2);
         }
         else if (Input.GetKey("left"))
         {
+            body.velocity = new Vector2(-moveSpeed, 0);
+            
             animator.SetBool(Walk, true);
             animator.SetInteger(Position, 3);
         }
         else
         {
             animator.SetBool(Walk, false);
+            body.velocity = Vector2.zero;
         }
     }
 }
---
+++ Assets/Scripts/CreateWorld.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWorld : MonoBehaviour
+{
+    public int columns = 17;
+    public int lines = 9;
+    public GameObject block;
+
+    private void Awake()
+    {
+        var camera = GetComponent<Camera>();
+        Vector3 worldPosition = camera.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, camera.nearClipPlane));
+
+        worldPosition.z = 0;
+        worldPosition.x += 0.5f;
+        worldPosition.y -= 0.5f;
+
+        // First Line
+        for (int i = 0; i < columns + 1; i++)
+        {
+            Instantiate(block, new Vector3(worldPosition.x + i, worldPosition.y, worldPosition.z), Quaternion.identity);
+        }
+
+        // Lines
+        for (int i = 0; i < lines - 1; i++)
+        {
+            Instantiate(block, new Vector3(worldPosition.x, worldPosition.y - i - 1, worldPosition.z), Quaternion.identity);
+            Instantiate(block, new Vector3(worldPosition.x + columns, worldPosition.y - i - 1, worldPosition.z), Quaternion.identity);
+        }
+
+        // Last line
+        for (int i = 0; i < columns + 1; i++)
+        {
+            Instantiate(block, new Vector3(worldPosition.x + i, worldPosition.y - lines, worldPosition.z), Quaternion.identity);
+        }
+
+        // Midle blocks
+        for (int i = 0; i < columns + 1; i++)
+        {
+            if (i == 0 || i == columns || i % 2 != 0)
+            {
+                continue;
+            }
+
+            for (int j = 0; j < lines - 1; j++)
+            {
+                if (j == 0 || j == columns || j % 2 == 0)
+                {
+                    continue;
+                }
+
+                Instantiate(block, new Vector3(worldPosition.x + i, worldPosition.y - j - 1, worldPosition.z), Quaternion.identity);
+            }
+        }
+
+    }
+}
